0f62dcd022f36bf8b55025c07804c3e07c47c247
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2024-05-24 00:32:25 +0300

    initialize

---
+++ usermicro/src/main/java/com/example/usermicro/usermicro/UsermicroApplication.java
@@ -0,0 +1,12 @@
+package com.example.usermicro.usermicro;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermicroApplication.class, args);
+	}
+
+}
---
+++ usermicro/src/test/java/com/example/usermicro/usermicro/UsermicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.usermicro.usermicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
